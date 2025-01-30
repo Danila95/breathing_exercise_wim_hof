80848a940ee6c7d5c3e3507deb75540580cc6fcb
ddb4 <ddb4@tpu.ru>
2025-01-30 17:21:35 +0700

    feat: big refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,105 @@
-import {useEffect, useRef, useState} from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import {Button} from "antd";
+import { Button, Slider } from 'antd'
 import { PlayCircleOutlined, PauseOutlined } from '@ant-design/icons'
 
 function App() {
-    const [isPlaying, setIsPlaying] = useState(false); // Состояние для отслеживания воспроизведения
-    const audioRef = useRef<HTMLAudioElement | null>(null); // Референс на аудиоплеер
-    const [countBreathes, setCountBreathes] = useState(0);
-    const [lastCount, setLastCount] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false) // Состояние для отслеживания воспроизведения
+	const [speedAudio, setSpeedAudio] = useState(1) // Скорость дыхания
+	const audioRef = useRef<HTMLAudioElement | null>(null) // Референс на аудиоплеер
+	const [countBreathes, setCountBreathes] = useState(0)
+	// const [lastCount, setLastCount] = useState(0)
 
-    useEffect(() => {
-        let intervalId;
+	useEffect(() => {
+		let intervalId: ReturnType<typeof setInterval>
 
-        if (isPlaying) {
-            intervalId = setInterval(() => {
-                setCountBreathes(prevNum => prevNum + 1);
-            }, 2500);
-        }
+		if (isPlaying) {
+			intervalId = setInterval(() => {
+				setCountBreathes(prevNum => prevNum + 1)
+			}, 2450 / speedAudio)
+		}
 
-        // Очистка эффекта при размонтировании компонента
-        return () => {
-            clearInterval(intervalId);
-            setLastCount(countBreathes);
-        };
-    }, [isPlaying]);
+		// Очистка эффекта при размонтировании компонента
+		return () => {
+			clearInterval(intervalId)
+			// setLastCount(countBreathes)
+		}
+	}, [countBreathes, isPlaying, speedAudio])
 
-    useEffect(() => {
-        if (countBreathes === 40) {
-            if ("pause" in audioRef.current) {
-                audioRef.current.pause();
-                audioRef.current.currentTime = 0;
-            }
-            setIsPlaying(false);
-        }
-    }, [countBreathes]);
+	useEffect(() => {
+		if (countBreathes === 40) {
+			if (audioRef.current) {
+				audioRef.current.pause()
+				audioRef.current.currentTime = 0
+				setCountBreathes(0)
+			}
+			setIsPlaying(false)
+		}
+	}, [countBreathes])
 
-    // Функция для переключения между воспроизведением и паузой
-    const handleStartBreathe = () => {
-        if (!isPlaying) {
-            if (audioRef.current) {
-                // Начинаем воспроизведение
-                if ("play" in audioRef.current) {
-                    audioRef.current.play();
-                }
-            }
-            setIsPlaying(true);
-        } else {
-            if (audioRef.current) {
-                // Ставим на паузу
-                if ("pause" in audioRef.current) {
-                    audioRef.current.pause();
-                    audioRef.current.currentTime = 0;
-                    setCountBreathes(0)
-                    setLastCount(0)
-                }
-            }
-            setIsPlaying(false);
-        }
-    }
+	useEffect(() => {
+		if (audioRef.current) {
+			audioRef.current.playbackRate = speedAudio
+		}
+	}, [speedAudio])
 
-    return (
-        <>
-            <Button
-                onClick={handleStartBreathe}
-                icon={isPlaying ? <PauseOutlined/> : <PlayCircleOutlined/>}
-                size='large'
-                iconPosition='end'
-            >
-                {isPlaying ? 'Stop' : 'Play'}
-            </Button>
-            <div>
-                Количество вдохов/выдохов: {countBreathes}
-            </div>
+	// Функция для переключения между воспроизведением и паузой
+	const handleStartBreathe = () => {
+		if (!isPlaying) {
+			if (audioRef.current) {
+				// Начинаем воспроизведение
+				if ('play' in audioRef.current) {
+					audioRef.current.play()
+				}
+			}
+			setIsPlaying(true)
+		} else {
+			if (audioRef.current) {
+				// Ставим на паузу
+				if ('pause' in audioRef.current) {
+					audioRef.current.pause()
+					audioRef.current.currentTime = 0
+					setCountBreathes(0)
+					// setLastCount(0)
+				}
+			}
+			setIsPlaying(false)
+		}
+	}
 
-            {/* Аудиоплеер */}
-            <audio ref={audioRef} src="/01_Marina1.m4a" hidden/>
-        </>
-    )
+	const handleSpeedAudio = (newValue: number) => {
+		setSpeedAudio(newValue)
+	}
+
+	return (
+		<>
+			<Button
+				onClick={handleStartBreathe}
+				icon={isPlaying ? <PauseOutlined /> : <PlayCircleOutlined />}
+				size='large'
+				iconPosition='end'
+			>
+				{isPlaying ? 'Stop' : 'Play'}
+			</Button>
+			{/* eslint-disable-next-line i18next/no-literal-string */}
+			<div>Количество вдохов/выдохов: {countBreathes}</div>
+			<Slider
+				min={0.9}
+				max={2}
+				step={0.2}
+				onChange={handleSpeedAudio}
+				value={speedAudio}
+			/>
+
+			{/* Аудиоплеер */}
+			{/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+			<audio
+				ref={audioRef}
+				src='/01_Marina1.m4a'
+				hidden
+			/>
+		</>
+	)
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,53 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-
-  return {
-    plugins: [],
-    resolve: {
-      alias: [
-        { find: '@', replacement: path.resolve(__dirname, './src') },
-        { find: '@public', replacement: path.resolve(__dirname, './public') },
-        // Подменяем "fs" на пустой модуль для подавления мусорных предупреждений в консоли devTools браузера
-        {
-          find: 'fs',
-          replacement: path.resolve(__dirname, 'src/emptyFsMock.js')
-        }
-      ]
-    },
-    build: {
-      outDir: 'build',
-      minify: 'esbuild',
-      rollupOptions: {
-        output: {
-          // Настройка каталога для файлов .js
-          entryFileNames: 'js/[name].js',
-          chunkFileNames: 'js/[name]-[hash].js',
-          assetFileNames: ({ names }) => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-            if (/\.css$/.test(names ?? '')) {
-              return 'css/[name]-[hash][extname]'
-            }
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-            if (/\.(gif|jpe?g|png|svg|webp)$/.test(names ?? '')) {
-              return 'assets/images/[name][extname]'
-            }
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-expect-error
-            if (/\.(woff|woff2|eot|ttf|otf)$/.test(names ?? '')) {
-              return 'assets/fonts/[name][extname]'
-            }
-            return 'assets/[name][extname]'
-          }
-        }
-      }
-    },
-    server: {
-      port: 3000,
-      open: true
-    },
-    define: {}
-  }
+export default defineConfig(() => {
+	return {
+		plugins: [],
+		resolve: {
+			alias: [
+				{ find: '@', replacement: path.resolve(__dirname, './src') },
+				{ find: '@public', replacement: path.resolve(__dirname, './public') },
+				// Подменяем "fs" на пустой модуль для подавления мусорных предупреждений в консоли devTools браузера
+				{
+					find: 'fs',
+					replacement: path.resolve(__dirname, 'src/emptyFsMock.js')
+				}
+			]
+		},
+		build: {
+			outDir: 'build',
+			minify: 'esbuild',
+			rollupOptions: {
+				output: {
+					// Настройка каталога для файлов .js
+					entryFileNames: 'js/[name].js',
+					chunkFileNames: 'js/[name]-[hash].js',
+					assetFileNames: ({ names }) => {
+						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+						// @ts-expect-error
+						if (/\.css$/.test(names ?? '')) {
+							return 'css/[name]-[hash][extname]'
+						}
+						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+						// @ts-expect-error
+						if (/\.(gif|jpe?g|png|svg|webp)$/.test(names ?? '')) {
+							return 'assets/images/[name][extname]'
+						}
+						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+						// @ts-expect-error
+						if (/\.(woff|woff2|eot|ttf|otf)$/.test(names ?? '')) {
+							return 'assets/fonts/[name][extname]'
+						}
+						return 'assets/[name][extname]'
+					}
+				}
+			}
+		},
+		server: {
+			port: 3000,
+			open: true
+		},
+		define: {}
+	}
 })
